ad1a4c7d921f5ff93d5f486d1f560d0417e6e8a2
Alex van Assem <avassem@gmail.com>
2016-10-27 16:31:12 +0200

    Add Gulp Deploy to RaspberryPi

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,19 @@
 const gulp    = require('gulp');
 const yaml    = require('gulp-yaml-validate');
+var replace = require('gulp-replace');
 
 gulp.task('yaml', () => {
   return gulp.src("**/*.yaml")
+    .pipe(replace('!secret ', ''))
+    .pipe(replace('!include_dir_merge_named ', ''))
+    .pipe(replace('!include ', ''))
+    .pipe(replace('!include_dir_named ', ''))
     .pipe(yaml());
 });
+
+gulp.task('deploy', function() {
+    gulp.src("**/*.yaml")
+    .pipe(gulp.dest('//RASPBERRYPI/home assistant'));
+});
+
 gulp.task('default', ['yaml'])
